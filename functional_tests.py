6c4c21be288f06c4cae59f199bb29cf1f70fd3d7
adu <aluta182004@gmail.com>
2022-08-27 12:58:58 +0000

    Functional tests now checks if we can enter an item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,8 @@
+from email import header
+import imp
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -14,8 +18,32 @@ class NewVisitorTest(unittest.TestCase):
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # open the web app
         self.browser.get('http://localhost:8000')
-        self.assertIn('To Do',self.browser.title)
+        # notices the title of the page
+        self.assertIn('To-Do',self.browser.title)
+        #notices the contents of the page
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # seeing a field with placeholder 'Enter a to-do item
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+        
+        # enter an item
+        input_box.send_keys('Push code to Github Repo')
+
+        # saves the item by tapping 'Enter'
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_id('tr')
+        self.assertTrue(
+            any(row.text == '1: Push code to Github Repo' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
